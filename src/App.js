1b856f95a1bc25a7a76f8662bb1efb8b3df3459f
Давид <davidshapkin@gmail.com>
2021-04-22 01:11:57 +0300

    Final Lesson 49.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 const App = () => {
     return (
             <div className='app-wrapper'>
                 <Header />
-                <Navbar />
+                <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
-                           render={ () => <DialogsContainer /> }/>
+                           render={() => <DialogsContainer/>}/>
 
                     <Route path='/profile'
-                           render={ () => <Profile /> }/>
+                           render={() => <Profile/>}/>
+
+                    <Route path='/users'
+                           render={() => <UsersContainer/>}/>
                 </div>
             </div>
         )
