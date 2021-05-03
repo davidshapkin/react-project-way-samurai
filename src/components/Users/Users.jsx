53e58d7b3843b219f2d810019a89b8c8c716c3bf
Давид <davidshapkin@gmail.com>
2021-05-03 12:53:30 +0300

    Final lesson 57.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import styles from './Users.module.css';
-import axios from "axios";
-import userPhoto from "../../assets/images/user.jpg"
+import styles from "./Users.module.css";
+import userPhoto from "../../assets/images/user.jpg";
 
-const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
-
-
+let Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    return (
+        <div>
+            <div>
+                {pages.map(p => {
+                    return <span className={props.currentPage === p && styles.selectedPage}
+                                 onClick={(e) => {
+                                     //props.setCurrentPage(p);
+                                     props.onPageChanged(p)
+                                 }}>{p}</span>
+                })}
+            </div>
+            {
+                props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
@@ -30,7 +37,7 @@ const Users = (props) => {
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -40,9 +47,10 @@ const Users = (props) => {
                         <div>{"u.location.country"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    )
 }
 
 export default Users;
