db3bc8ead6345d90754db73255f819427277d952
Давид <davidshapkin@gmail.com>
2021-05-21 13:03:38 +0300

    Final lesson 63.

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "cd8a4881-a219-4714-a22a-51b75f27cf13"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data);
+    }
+}
+
+
+
